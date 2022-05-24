50072a15b31d6521ff01772a23840cc6628a46c5
Ang Li <angl@princeton.edu>
2022-05-24 13:31:25 -0400

    [Misc] Cleaning up duet dir

--- src/duet/DuetYAMIFE.py
+++
@@ -1,9 +0,0 @@
-from m5.params import *
-from m5.objects.ClockedObject import ClockedObject
-
-class DuetYAMIFE (ClockedObject):
-    type        = "DuetYAMIFE"
-    cxx_class   = "gem5::duet::DuetYAMIFE"
-    cxx_header  = "duet/DuetYAMIFE.hh"
-
-    outport     = RequestPort ("Output port")
